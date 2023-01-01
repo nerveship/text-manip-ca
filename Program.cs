74f42fa4802023de90231361a1f7fdcaef100781
Johnny <johnstuartgonsales@gmail.com>
2023-01-01 07:19:36 +0000

    check user input

--- Program.cs
+++ Program.cs
@@ -4,10 +4,20 @@ public class Program
 {
     public static void Main()
     {
+        int choiceMax = 4;
+
         Console.WriteLine("Please pick how you would like to modify your text.\n" +
             "1. Reverse\n" +
             "2. Capitalise\n" +
             "3. Lowercase\n" +
             "4. Remove spaces\n");
+
+        int usrChoice = Convert.ToInt32(Console.ReadLine());
+        
+        while (usrChoice > choiceMax || usrChoice < 1)
+        {
+            Console.WriteLine($"Please choose a number between 1 and {choiceMax}");
+            usrChoice = Convert.ToInt32(Console.ReadLine());
+        }
     }
 }
