cb952c883a0a206787f0fb4300bff0f00e3ed800
Johnny <johnstuartgonsales@gmail.com>
2023-01-01 07:13:03 +0000

    prompt user for input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please pick how you would like to modify your text.\n" +
+            "1. Reverse\n" +
+            "2. Capitalise\n" +
+            "3. Lowercase\n" +
+            "4. Remove spaces\n");
+    }
+}
